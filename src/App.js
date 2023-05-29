9d71ed6a2964b922c3f0c48d89edc20f215694ef
Luyen Dinh <luyen1368@gmail.com>
2023-05-29 16:59:29 +0700

    design trello

--- src/App.js
+++ src/App.js
@@ -1,7 +1,104 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return <div className="App">oke</div>;
+  return (
+    <div className="trello">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://phuongtanphuoc.gov.vn/wp/vietnam/anhdepvietnam%20(32).jpg"
+                alt=""
+              />
+              Title: Today
+            </li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://phuongtanphuoc.gov.vn/wp/vietnam/anhdepvietnam%20(32).jpg"
+                alt=""
+              />
+              Title: Today
+            </li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://phuongtanphuoc.gov.vn/wp/vietnam/anhdepvietnam%20(32).jpg"
+                alt=""
+              />
+              Title: Today
+            </li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://phuongtanphuoc.gov.vn/wp/vietnam/anhdepvietnam%20(32).jpg"
+                alt=""
+              />
+              Title: Today
+            </li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://phuongtanphuoc.gov.vn/wp/vietnam/anhdepvietnam%20(32).jpg"
+                alt=""
+              />
+              Title: Today
+            </li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+            <li>what are you doing</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
